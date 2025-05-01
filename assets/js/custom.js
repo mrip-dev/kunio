42b869b6dfe946427c7e5df6c8e74181ce66b2e5
IQBAL-KAMBOH <theiqbal111@gmail.com>
2025-05-01 08:13:55 +0500

    Header set

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 (function ($) {
-	
+
 	"use strict";
 
 	// Window Resize Mobile Menu Fix
@@ -8,15 +8,23 @@
 
 	// Scroll animation init
 	window.sr = new scrollReveal();
-	
+
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
-		$(".menu-trigger").on('click', function() {	
+		$(".menu-trigger").on('click', function() {
 			$(this).toggleClass('active');
 			$('.header-area .nav').slideToggle(200);
 		});
 	}
+	//// On scroll add main-nav class scrolled
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() > 50) {
+			$('.header-area').addClass('scrolled');
+		} else {
+			$('.header-area').removeClass('scrolled');
+		}
+	});
 
 
 	// Menu elevator animation
@@ -28,8 +36,8 @@
 				var width = $(window).width();
 				if(width < 991) {
 					$('.menu-trigger').removeClass('active');
-					$('.header-area .nav').slideUp(200);	
-				}				
+					$('.header-area .nav').slideUp(200);
+				}
 				$('html,body').animate({
 					scrollTop: (target.offset().top) - 130
 				}, 700);
@@ -40,17 +48,17 @@
 
 	$(document).ready(function () {
 	    $(document).on("scroll", onScroll);
-	    
+
 	    //smoothscroll
 	    $('a[href^="#"]').on('click', function (e) {
 	        e.preventDefault();
 	        $(document).off("scroll");
-	        
+
 	        $('a').each(function () {
 	            $(this).removeClass('active');
 	        })
 	        $(this).addClass('active');
-	      
+
 	        var target = this.hash,
 	        menu = target;
 	       	var target = $(this.hash);
